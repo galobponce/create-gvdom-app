bb6e7a826c23d9b55c5d7d5e6b756ae372e470b6
galobponce <personal.galo.bello@gmail.com>
2023-06-02 15:52:36 -0300

    Add gvdom initial app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+import { gvdom, createElement } from "gvdom"
+
+const Title = ({ text }) => {
+  return (
+    <h1 class="title" onClick={() => console.log("Hello World!")}>
+      {text}
+    </h1>
+  )
+}
+
+document
+  .getElementById("app")
+  .appendChild(createElement(<Title text="My First Gvdom App" />))
